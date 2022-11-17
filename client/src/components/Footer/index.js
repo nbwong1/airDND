4358976f70ccd2dd3cfa6b26d03f7c99de18e8e1
Kevin Umayam <kevin.m.umayam@gmail.com>
2022-11-17 15:17:49 -0800

    a terrible navbar

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,32 +1,60 @@
-import React from 'react';
-import { useLocation, useNavigate } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
+import "./footer.css";
+import Auth from "../../utils/auth";
+
+const styles = {
+  MarginB: {
+    marginBottom: "0px",
+  },
+  Padding: {
+    padding: " padding: 10px 10px 10px 10px black solid",
+  },
+};
 
 const Footer = () => {
-  const location = useLocation();
-  const navigate = useNavigate();
+  const logout = (event) => {
+    event.preventDefault();
+    Auth.logout();
+  };
   return (
-    <footer className="w-100 mt-auto bg-secondary p-4">
-      <div className="container text-center mb-5">
-        {location.pathname !== '/' && (
-          <button
-            className="btn btn-dark mb-3"
-            onClick={() => navigate(-1)}
-          >
-            &larr; Go Back
-          </button>
+    <footer style={styles.marginBottom} className="">
+      <div className="">
+        {Auth.loggedIn() ? (
+          <>
+            <div className=" alignDivs">
+              <div className="sideDiv">
+                <Link className="" to="/">
+                  <h1 className=" bNavT">Campains </h1>
+                </Link>
+              </div>
+
+              <div style={styles.Padding} className="middleDiv alignDiv ">
+                <Link className="alignDiv" to="/me">
+                  <h1 className="bNavTM alignDiv">
+                    {" "}
+                    {Auth.getProfile().data.username}'s profile
+                  </h1>
+                </Link>
+              </div>
+
+              <div className="sideDiv">
+                <h1 className="bNavT" onClick={logout}>
+                  Logout
+                </h1>
+              </div>
+            </div>
+          </>
+        ) : (
+          <>
+            <Link className="btn btn-lg btn-info m-2" to="/login">
+              Login
+            </Link>
+            <Link className="btn btn-lg btn-light m-2" to="/signup">
+              Signup
+            </Link>
+          </>
         )}
-        <h4>
-          Made with{' '}
-          <span
-            className="emoji"
-            role="img"
-            aria-label="heart"
-            aria-hidden="false"
-          >
-            ❤️
-          </span>{' '}
-          by the Tech Thoughts team.
-        </h4>
       </div>
     </footer>
   );
