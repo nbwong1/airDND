cd203712d1c1debcdb4a1035394a46c3fdfc4cc8
andy-sd-858 <andy.sd.858@gmail.com>
2022-11-22 20:12:41 -0800

    updated headers
    stylized the headers and changed some things

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
 import Auth from '../../utils/auth';
+
 
 const Header = () => {
   const logout = (event) => {
@@ -13,9 +13,9 @@ const Header = () => {
       <div className="container flex-row justify-space-between-lg justify-center align-center">
         <div>
           <Link className="text-light" to="/">
-            <h1 className="m-0">Tech Thoughts</h1>
+            <h1 className="m-0">airDnD</h1>
           </Link>
-          <p className="m-0">Get into the mind of a programmer.</p>
+          <p className="m-0">DnD Time!.</p>
         </div>
         <div>
           {Auth.loggedIn() ? (
@@ -30,7 +30,7 @@ const Header = () => {
           ) : (
             <>
               <Link className="btn btn-lg btn-info m-2" to="/login">
-                Login
+                Log-in
               </Link>
               <Link className="btn btn-lg btn-light m-2" to="/signup">
                 Signup
