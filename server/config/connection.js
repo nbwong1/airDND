fdb5b09d543bdaefe6d61acfacc0491d9476ec24
Kevin Umayam <kevin.m.umayam@gmail.com>
2022-11-29 12:35:31 -0800

    testin

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/airDnD",
+  process.env.MONGODB_URI || "mongodb://localhosts/airDnD",
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
