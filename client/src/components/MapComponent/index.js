942d695d70e5a0dce46ecd56c46d9c0f16ce98eb
digibrill <digibrill@users.noreply.github.com>
2022-11-28 23:06:08 -0800

    Add files via upload

--- client/src/components/MapComponent/index.js
+++ client/src/components/MapComponent/index.js
@@ -1,9 +1,20 @@
-import React from "react";
+import React from "react";//, { useState } 
 import "./index.css";
 import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps";
+// import Geocode from "react-geocode";
+// Geocode.setApiKey("AIzaSyDydXgSD9aepywJ9KlnLI_iHZJkNpUm84M");
+// let newlat;
+// let newlng;
+// Geocode.fromAddress('950 f ave coronado').then(
+//   (response) => {
+//     const { lat, lng } = response.results[0].geometry.location;
+//     newlat = lat;
+//     newlng = lng;
+//   }
+// )
+// console.log(newlat, newlng);
 const MapComponent = withScriptjs(
     withGoogleMap((props) => {
-        console.log(props.passedlat, props.passedlng);
         return (
                 <GoogleMap
                 defaultZoom={15}
@@ -14,5 +25,5 @@ const MapComponent = withScriptjs(
         )
     })
 );
-//lat: 32.71500, lng: -117.16250 - SAN DIEGO
+//lat: 32.71500, lng: -117.16250
 export default MapComponent;
