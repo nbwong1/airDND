1156eb8fc2c5f2b114a32b10f367932486b8123f
Kevin Umayam <kevin.m.umayam@gmail.com>
2022-11-29 16:53:31 -0800

    testing

--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ const db = require("./config/connection");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+require("dotenv").config({ path: path.join(__dirname, ".env") });
 const server = new ApolloServer({
   typeDefs,
   resolvers,
